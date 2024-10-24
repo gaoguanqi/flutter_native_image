064404b273c42acff669b0f7fc064adcb159a98a
高冠祺 <307590625@qq.com>
2024-10-24 10:44:18 +0800

    code

--- example/android/app/src/main/java/com/example/flutternativeimageexample/MainActivity.java
+++ example/android/app/src/main/java/com/example/flutternativeimageexample/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.flutternativeimageexample;
 
 import android.os.Bundle;
 
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+//    GeneratedPluginRegistrant.registerWith(Objects.requireNonNull(this.getFlutterEngine()));
   }
 }
